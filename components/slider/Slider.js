2e9ed610bee9658fbe0a0f3aa1a9e1f41e3e52bb
Allllakis <allakiselova20@gmail.com>
2021-12-26 17:15:57 +0300

    adding slider functionality

--- components/slider/Slider.js
+++ components/slider/Slider.js
@@ -1,16 +1,32 @@
 import { useState } from "react";
-import Image from 'next/image'
-import React from "react";
+
 import classes from "./Slider.module.css";
-// import { images } from "./DataSlider.js";
-import {images} from './DataSlider.js';
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowBackIos";
+import { images } from "./DataSlider.js";
 
 const Slider = () => {
- const [currImg, setCurrImg] = useState(0)
-  
+  const [currImg, setCurrImg] = useState(0);
+
   return (
     <div className={classes.slider}>
-      <div className={classes.sliderInner} style={{backgroundImage: `url(${images[currImg].image})`}}>
+      <div
+        className={classes.sliderInner}
+        style={{ backgroundImage: `url(${images[currImg].image})` }}
+      >
+        <div
+          className={classes.left}
+          onClick={() => currImg > 0 && setCurrImg(currImg - 1)}
+        >
+          <ArrowBackIosIcon />
+        </div>
+        <div className={classes.center}></div>
+        <div
+          className={classes.right}
+          onClick={() => currImg < images.length - 1 && setCurrImg(currImg + 1)}
+        >
+          <ArrowForwardIosIcon />
+        </div>
         {/* <img src={images[currImg].image} /> */}
         {/* <Image src={images[currImg].image} alt='villa' layout='fill'/> */}
       </div>
