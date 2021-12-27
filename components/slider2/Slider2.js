9d202e4710f253980cd605f919b576d4659cbff7
Allllakis <allakiselova20@gmail.com>
2021-12-27 18:36:47 +0300

    add swiper slider react

--- components/slider2/Slider2.js
+++ components/slider2/Slider2.js
@@ -1,49 +1,71 @@
 import { useState } from "react";
-import { Swiper, SwiperSlide, Parallax } from "swiper/react";
-import SwiperCore from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Scrollbar, Parallax, Navigation } from "swiper";
+import "swiper/css/scrollbar";
+import "swiper/css/pagination";
 import "swiper/css/bundle";
 import "swiper/css";
 
 import { images } from "../slider/DataSlider";
-import classes from './Slider2.module.css'
+import classes from "./Slider2.module.css";
+
+SwiperCore.use([Parallax, Scrollbar, Navigation]);
+
+const SliderConfigs = {
+  parallax: true,
+  centeredSlides: true,
+  grabCursor: true,
+  speed: 2400,
+  spaceBetween: 0,
+  effect: "slide",
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: "swiper-button-next",
+  },
+};
 
 const Slider2 = () => {
-    const [currImg, setCurrImg] = useState(0);
-  const slides = [];
-
-  for (let i = 0; i < 3; i += 1) {
-    slides.push(
-      <SwiperSlide key={`slide${i}`}>
-        <div
-          className={classes.sliderInner}
-          style={{ backgroundImage: `url(${images[currImg].image})` }}
-        ></div>
-      </SwiperSlide>
-    );
-  }
-
   return (
     <div className={classes.slider}>
-      <Swiper id="main">{slides}</Swiper>
+      <Swiper
+        {...SliderConfigs}
+        slidesPerView={1}
+        loop={true}
+        navigation
+        scrollbar={{
+          draggable: true,
+          dragSize: 500,
+        }}
+      >
+        <SwiperSlide>
+          <div
+            className={classes.sliderInner}
+            data-swiper-parallax="20%"
+            style={{ backgroundImage: `url(${images[0].image})` }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className={classes.sliderInner}
+            style={{ backgroundImage: `url(${images[1].image})` }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className={classes.sliderInner}
+            style={{ backgroundImage: `url(${images[2].image})` }}
+          ></div>
+        </SwiperSlide>
+        {/* <SwiperSlide>
+      <div className={classes['slider-scrollbar']}>
+        <div className={classes['swiper-scrollbar']}>
+
+        </div>
+      </div>
+    </SwiperSlide> */}
+      </Swiper>
     </div>
   );
 };
 
 export default Slider2;
-
-// export default () => {
-//   return (
-//     <Swiper
-//       spaceBetween={50}
-//       slidesPerView={3}
-//       onSlideChange={() => console.log('slide change')}
-//       onSwiper={(swiper) => console.log(swiper)}
-//     >
-//       <SwiperSlide>Slide 1</SwiperSlide>
-//       <SwiperSlide>Slide 2</SwiperSlide>
-//       <SwiperSlide>Slide 3</SwiperSlide>
-//       <SwiperSlide>Slide 4</SwiperSlide>
-//       ...
-//     </Swiper>
-//   );
-// };
